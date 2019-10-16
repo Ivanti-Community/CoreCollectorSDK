f17804abc56b8440a018b47f7d9e19d5b4b7c9b8
roy.morris@ivanti.com <roy.morris@ivanti.com>
2019-10-16 07:37:40 -0700

    Added local nuget packaging.

--- Build/Build-SDK.ps1
+++ Build/Build-SDK.ps1
@@ -1,10 +1,21 @@
 ﻿<#
-	Builds the SDK
+ ---------------------------------------------------------------------------
+  Copyright (C) Ivanti Corporation 2019. All rights reserved.
+ 
+  This file contains trade secrets of the Ivanti Corporation. No part
+  may be reproduced or transmitted in any form by any means or for any purpose
+  without the express written permission of the Ivanti Corporation.
+ ---------------------------------------------------------------------------
+ Notes: Builds the Core Collector SDK soulution.
+ Author: Roy Morris roy.morris@ivanti.com
 #>
 
 param
 (
-[String] $config = "Debug"
+[Bool] $restore = $TRUE,   
+[String] $config = "Debug",
+[Boolean] $packageLocally = $FALSE,
+[String] $version = "3.0.0"
 )
 
 $lastDir = Get-Location
@@ -28,9 +39,11 @@ Set-Location -Path "$workingDir"
 [Environment]::CurrentDirectory = $workingDir
 
 ###################### Restore Nuget Packages ###################################
-Write-Host "Restoring nuget packages: local repo : $nugetLocalRepo"
-Start-Process "$nugetExe" "restore" -Wait -NoNewWindow
-
+if ($restore) 
+{
+    Write-Host "Restoring nuget packages: local repo : $nugetLocalRepo"
+    Start-Process "$nugetExe" "restore" -Wait -NoNewWindow
+}
 ###################### Build the SDK ############################################
 $solutionFile = "$parentDir\Src\CoreCollectorSDK.sln"
 Write-Host "Solution file: " $solutionFile
@@ -38,6 +51,29 @@ Write-Host "Solution file: " $solutionFile
 Write-Host "Building Collector SDK"
 Write-Host $msbuild $solutionFile /t:Rebuild /fl /clp:"v=m;Summary" /p:Configuration=$config /p:Platform="Any CPU"
 & $msbuild $solutionFile /t:Rebuild /fl /clp:"v=m;Summary" /p:Configuration=$config /p:Platform="Any CPU"
+
+if ($packageLocally) 
+{
+    ###################### Create Nuget Package #####################################
+    Write-Host "Creating Collector SDK Nuget Package..."
+    $collectorSdkDir = "$workingDir\Core.Collector.SDK"
+    $libDir = "$collectorSdkDir\lib"
+    $frameworkDir = "$libDir\netcoreapp2.2"
+    $binDir = "$collectorSdkDir\obj\$config\netcoreapp2.2"
+
+    if (!(Test-Path -Path $libDir))
+    {
+        [System.IO.Directory]::CreateDirectory($libDir) > $null
+    }
+    if (!(Test-Path -Path $frameworkDir))
+    {
+        [System.IO.Directory]::CreateDirectory($frameworkDir) > $null
+    }
+
+    Copy-Item -Path "$binDir\Core.Collector.SDK.dll" -Destination $frameworkDir -Force
+    Start-Process "$nugetExe" "pack $collectorSdkDir\Core.Collector.SDK.nuspec -Version $version" -Wait -NoNewWindow
+    Copy-Item -Path "$workingDir\Ivanti.Core.Collector.SDK.$version.nupkg" -Destination $nugetLocalRepo -Force
+}
 
 Set-Location $lastDir
 
