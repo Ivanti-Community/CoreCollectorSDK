9c09a96dd1424f73825816c647bacc090ddbcbcf
roy.morris@ivanti.com <roy.morris@ivanti.com>
2019-10-17 17:39:51 -0700

    Fixed Simple Date Time UTC converter.

--- Src/Core.Collector.SDK.Samples/Converters/SimpleDateTimeUtcConverter.cs
+++ Src/Core.Collector.SDK.Samples/Converters/SimpleDateTimeUtcConverter.cs
@@ -3,6 +3,7 @@
 // ***************************************************************
 using Collector.SDK.Converters;
 using Collector.SDK.DataModel;
+using Collector.SDK.Logging;
 using System;
 using System.Collections.Generic;
 
@@ -10,17 +11,36 @@ namespace Collector.SDK.Samples.Converters
 {
     public class SimpleDateTimeUtcConverter : AbstractConverter
     {
+        private readonly ILogger _logger;
+
+        public SimpleDateTimeUtcConverter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public override Dictionary<string, object> Convert(KeyValuePair<string, object> point, IEntityCollection data)
         {
             var result = new Dictionary<string, object>();
-            var names = ConvertLeftSide(point.Key);
-            foreach (var name in names)
-            {
-                DateTime parsedDateTime;
-                if (DateTime.TryParse((string)point.Value, out parsedDateTime))
+            try {
+                DateTime utcDateTime = DateTime.Now.ToUniversalTime();
+                if (point.Value is DateTime)
                 {
-                    result.Add(name, parsedDateTime.ToUniversalTime());
+                    utcDateTime = ((DateTime)point.Value).ToUniversalTime();
                 }
+                else
+                {
+                    utcDateTime = DateTime.Parse(point.Value as string).ToUniversalTime();
+                }
+                var names = ConvertLeftSide(point.Key);
+                foreach (var name in names)
+                {
+                    result.Add(name, utcDateTime);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e.Message);
+                return result;
             }
             return result;
         }
